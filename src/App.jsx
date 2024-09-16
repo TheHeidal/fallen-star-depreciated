1675010bc839e4f9507c0f4d73ab1d0dbbe7edb2
Mars <theHeidal@gmail.com>
2024-09-15 18:48:25 -0700

    Trying out react-tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import "./App.css";
-import { useState } from "react";
-import tabs
+import "react-tabs/style/react-tabs.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import WizardList from "./components/WizardList";
-import TabList from "./components/TabList";
 import WarlockSummary from "./components/WarlockSummary";
 
-function App(props) {
-  const [currentTab, setTab] = useState("summary");
+function App() {
   return (
     <>
       <div className="wrapper">
         <WizardList />
-        <TabList currentTab={currentTab} />
-        <div className="content">
-          <WarlockSummary />
-        </div>
+
+        <Tabs>
+          <TabList>
+            <Tab>Character Sheet</Tab>
+            <Tab>Board</Tab>
+            <Tab>Codex</Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Ur the faustian</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Ur losing</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Don't lose!</h2>
+          </TabPanel>
+        </Tabs>
+        <div className="content"></div>
       </div>
     </>
   );
