30345d6ee6227e43d327f4513938a3347f4b2f6e
Mars <theHeidal@gmail.com>
2024-09-15 18:30:43 -0700

    Laid out the warlock summary

---
+++ src/components/TabList.jsx
@@ -0,0 +1,15 @@
+import "./TabList.css";
+
+function TabList() {
+  return (
+    <div className="tabs-container">
+      <ul>
+        <li>Summary</li>
+        <li>Board</li>
+        <li>Codex</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TabList;
---
+++ src/components/WizardList.jsx
@@ -0,0 +1,19 @@
+import "./WizardList.css";
+function WizardList() {
+  return (
+    <div className="header-container">
+      <ul>
+        <li>Orrery</li>
+        <li>Necromancer</li>
+        <li>Hierophant</li>
+        <li>Hierophant</li>
+        <li>Hierophant</li>
+        <li>Hierophant</li>
+        <li>Hierophant</li>
+        <li>Hierophant</li>
+      </ul>
+    </div>
+  );
+}
+
+export default WizardList;
